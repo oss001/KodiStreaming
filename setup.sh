a904081f0add1cb2ed886d57434c851a684a0377
Oss <oss001@hotmail.com>
2019-07-26 18:21:23 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo "Starting setup, this will only work on Kodi V18 or greater..."
+echo "Loading dependencies..."
+apt-get install python-pip python-crypto build-essential -y > /dev/null
+apt-get install python-all-dev python-setuptools python-wheel -y > /dev/null
+apt-get install python-crypto-dbg python-crypto-doc python-pip-whl -y > /dev/null
+echo "Complete."
+echo "Setting up Pycryptodome..."
+pip install pycryptodomex > /dev/null
+ln -s /usr/lib/python2.7/dist-packages/Crypto /usr/lib/python2.7/dist-packages/Cryptodome
+echo "Complete."
+echo "Getting Netflix Repository..."
+mkdir ~/addons
+cd ~/addons
+wget https://github.com/castagnait/repository.castagnait/raw/master/repository.castagnait-1.0.0.zip
+echo "Complete."
+echo "Getting Amazon Reporitory..."
+wget https://github.com/Sandmann79/xbmc/releases/download/v1.0.2/repository.sandmann79.plugins-1.0.2.zip
+echo "Complete."
+echo "Downloads complete. Please install via Kodi Addons from zip.  Located in Home/addons"
+echo "Reboot in"
+echo "5"
+sleep 1
+echo "4"
+sleep 1
+echo "3"
+sleep 1
+echo "2"
+sleep 1
+echo "1"
+sleep 1
+reboot
